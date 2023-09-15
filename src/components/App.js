b52ced86d3f4e755ce3b05833598c270c2c15b6e
unknown <stephenoluwafemi52@gmail.comm>
2023-09-15 15:05:05 +0100

    Done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -36,3 +36,16 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// In the App component, you're using the Container component twice with different sets of props and children:
+
+// In the first Container, you provide a header prop with the value "Example header" and a direction prop with the value "horizontal." You also include multiple <div> elements as children.
+
+// In the second Container, you provide a header prop with the value "Example header," a textPosition prop with the value "center," and you include multiple <img> elements as children.
+
+// The Container component uses these props to style and arrange its content based on the specified configuration.
+
+// Overall, the Container component is a flexible container that can adjust its styling and layout based on the provided props, making it versatile for various use cases.
